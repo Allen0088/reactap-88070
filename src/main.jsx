f5cbc79a4e69c8f18d8ef7bb883a9ed820d38a80
Allen0088 <eliasrivero814@gmail.com>
2025-10-31 21:35:32 -0300

    arreglo api

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { initializeApp } from "firebase/app";
-import App from './App.jsx'
-
+// src/main.jsx
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOUVyM7WgimOqQDw2dsO1mQxZi9ag7dvI",
-  authDomain: "venusapp-cb3af.firebaseapp.com",
-  projectId: "venusapp-cb3af",
-  storageBucket: "venusapp-cb3af.firebasestorage.app",
-  messagingSenderId: "303901164771",
-  appId: "1:303901164771:web:7ab5b64a709ea3810531d6"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 initializeApp(firebaseConfig);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />);
