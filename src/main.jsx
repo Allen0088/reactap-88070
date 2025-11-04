759f2b365b11e8580798785a077493acce0a37b0
Allen0088 <eliasrivero814@gmail.com>
2025-11-04 19:27:10 -0300

    pdddpd

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,22 @@ import App from './App.jsx';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
-  appId: import.meta.env.VITE_APP_ID
-};
-
 console.log("🔥 Firebase Config:", {
   apiKey: import.meta.env.VITE_API_KEY,
   projectId: import.meta.env.VITE_PROJECT_ID
 });
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0UVyM7WgIm0QQDw2ds01mQxZ19ag7dVI",
+  authDomain: "venusapp-cb3af.firebaseapp.com",
+  projectId: "venusapp-cb3af",
+  storageBucket: "venusapp-cb3af.appspot.com",
+  messagingSenderId: "383901164771",
+  appId: "1:383901164771:web:7ab5b64a709ea3810531d6"
+};
+
+console.log("FORZADO:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
